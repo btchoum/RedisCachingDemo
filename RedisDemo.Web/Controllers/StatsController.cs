89118c65c34670891404d5ddec132cd80920cc1e
btchoum <bertrand.tchoumkeu@gmail.com>
2020-11-27 08:45:49 -0600

    caching works !!!

---
+++ RedisDemo.Web/Controllers/StatsController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedisDemo.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatsController : ControllerBase
+    {
+        private readonly IDistributedCache _cache;
+
+        public StatsController(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        // GET api/stats
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> GetAsync()
+        {
+            var result = await GetStats();
+
+            return Ok(result);
+        }
+
+        private async Task<IEnumerable<string>> GetStats()
+        {
+            string cacheKey = "redis/demo/values";
+
+            var encodedBytes = await _cache.GetAsync(cacheKey);
+            if (encodedBytes != null)
+            {
+                var stringValue = Encoding.UTF8.GetString(encodedBytes);
+                return JsonConvert.DeserializeObject<string[]>(stringValue);
+            }
+
+            var stats = GetStatsFromDatabase();
+
+            var jsonString = JsonConvert.SerializeObject(stats);
+            var options = new DistributedCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromSeconds(60));
+
+            await _cache.SetStringAsync(cacheKey, jsonString, options);
+
+            return stats;
+        }
+
+        private static IEnumerable<string> GetStatsFromDatabase()
+        {
+            Thread.Sleep(1000);
+
+            return new[]
+            {
+                "stat 1",
+                "stat 2",
+                "stat 3",
+            };
+        }
+    }
+}
--- RedisDemo.Web/Startup.cs
+++ RedisDemo.Web/Startup.cs
@@ -19,6 +19,12 @@ namespace RedisDemo.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = "localhost";
+                options.InstanceName = "SampleInstance";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
