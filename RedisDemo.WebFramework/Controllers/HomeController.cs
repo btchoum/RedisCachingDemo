3120559017a5123f99e00189241d090888ea0ca4
btchoum <bertrand.tchoumkeu@gmail.com>
2020-11-28 14:06:20 -0600

    we can use StackExchangeRedis in .NET Framework

---
+++ RedisDemo.WebFramework/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace RedisDemo.WebFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
